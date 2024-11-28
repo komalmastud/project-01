b1da1d6fe457a428372ac39a5f117d1d7a5c5164
komalmastud7970@gmail.com <komalmastud7970@gmail.com>
2024-11-28 23:02:04 +0530

    working with Postman

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const fs = require("fs");
 const users = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 8000;
 
+//Middleware - Plugins
+app.use(express.urlencoded({ extended: false }));
 app.get("/users", (req, res) => {
   const html = `
     <ul>
@@ -33,8 +36,11 @@ app
   });
 
 app.post("/api/users", (req, res) => {
-  //TODO:Create new user
-  return res.json({ status: "pending" });
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "sucess!", id: users.length });
+  });
 });
 
 // Start the server
